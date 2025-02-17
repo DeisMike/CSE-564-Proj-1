5e2d42136e95007ee9563ca3e0b6d78b2217b1b1
Michael Deisler <mjdeisler@gmail.com>
2025-02-17 15:48:21 -0500

    Update options for vars, adjust axis label position

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 /* script.js */
 const width = 800;
 const height = 500;
-const margin = { top: 50, right: 50, bottom: 50, left: 50 };
+const margin = { top: 50, right: 100, bottom: 70, left: 100 };
 const innerWidth = width - margin.left - margin.right;
 const innerHeight = height - margin.top - margin.bottom;
 
@@ -13,6 +13,15 @@ const svg = d3.select("#chart")
     .attr("transform", `translate(${margin.left},${margin.top})`);
 
 let orientation = "upright";
+
+// Variables that should use a logarithmic scale due to high skewness
+const logScaleVariables = [
+    "Civilian_labor_force_2020", "Employed_2020", "Unemployed_2020", 
+    "Less than high school graduate, 2019-23", "High school graduate (or equivalency), 2019-23",
+    "Some college or associate degree, 2019-23", "Bachelor's degree or higher, 2019-23",
+    "CENSUS_2020_POP", "Joseph Biden Votes, 2020", "Donald Trump Votes, 2020", 
+    "Jo Jorgensen Votes, 2020", "Green Party Votes, 2020", "Other Votes, 2020", "Total Votes, 2020"
+];
 
 d3.csv("FINAL CSE 564 Proj 1 Dataset.csv").then(data => {
     const variableSelect = d3.select("#variable-select");
@@ -32,7 +41,7 @@ d3.csv("FINAL CSE 564 Proj 1 Dataset.csv").then(data => {
     });
 
     axisSelection.on("change", function() {
-        updateScatterplot();
+        // Do not update scatterplot here; wait for variable selection
     });
 
     function updateChart() {
@@ -40,8 +49,10 @@ d3.csv("FINAL CSE 564 Proj 1 Dataset.csv").then(data => {
 
         if (selectedVariable === "state_po") {
             drawBarChart(data);
+        } else if (logScaleVariables.includes(selectedVariable)) {
+            drawHistogram(data, true); // Use log scale
         } else {
-            drawHistogram(data);
+            drawHistogram(data, false); // Use linear scale
         }
     }
 
@@ -69,7 +80,7 @@ d3.csv("FINAL CSE 564 Proj 1 Dataset.csv").then(data => {
         svg.append("g")
             .call(yAxis);
 
-        const bars = svg.selectAll(".bar")
+        svg.selectAll(".bar")
             .data(states)
             .enter()
             .append("rect")
@@ -84,7 +95,7 @@ d3.csv("FINAL CSE 564 Proj 1 Dataset.csv").then(data => {
         svg.append("text")
             .attr("class", "axis-title")
             .attr("x", innerWidth / 2)
-            .attr("y", innerHeight + margin.bottom)
+            .attr("y", innerHeight + margin.bottom - 10)
             .style("text-anchor", "middle")
             .text("State ID Code");
 
@@ -92,12 +103,12 @@ d3.csv("FINAL CSE 564 Proj 1 Dataset.csv").then(data => {
             .attr("class", "axis-title")
             .attr("transform", `rotate(-90)`)
             .attr("x", -innerHeight / 2)
-            .attr("y", -margin.left)
+            .attr("y", -margin.left + 20)
             .style("text-anchor", "middle")
             .text("Number of Counties");
     }
 
-    function drawHistogram(data) {
+    function drawHistogram(data, useLogScale) {
         const values = data.map(d => +d[selectedVariable]).filter(d => !isNaN(d));
         const bins = d3.bin()
             .domain([d3.min(values), d3.max(values)])
@@ -107,8 +118,8 @@ d3.csv("FINAL CSE 564 Proj 1 Dataset.csv").then(data => {
             .domain([d3.min(values), d3.max(values)])
             .range(orientation === "upright" ? [0, innerWidth] : [0, innerHeight]);
 
-        const yScale = d3.scaleLinear()
-            .domain([0, d3.max(bins, d => d.length)])
+        const yScale = useLogScale ? d3.scaleLog() : d3.scaleLinear();
+        yScale.domain([1, d3.max(bins, d => d.length)]) // Log scale starts at 1
             .range(orientation === "upright" ? [innerHeight, 0] : [innerWidth, 0]);
 
         const xAxis = orientation === "upright" ? d3.axisBottom(xScale) : d3.axisLeft(xScale);
@@ -121,7 +132,7 @@ d3.csv("FINAL CSE 564 Proj 1 Dataset.csv").then(data => {
         svg.append("g")
             .call(yAxis);
 
-        const bars = svg.selectAll(".bar")
+        svg.selectAll(".bar")
             .data(bins)
             .enter()
             .append("rect")
@@ -136,7 +147,7 @@ d3.csv("FINAL CSE 564 Proj 1 Dataset.csv").then(data => {
         svg.append("text")
             .attr("class", "axis-title")
             .attr("x", innerWidth / 2)
-            .attr("y", innerHeight + margin.bottom)
+            .attr("y", innerHeight + margin.bottom - 10)
             .style("text-anchor", "middle")
             .text(selectedVariable);
 
@@ -144,9 +155,9 @@ d3.csv("FINAL CSE 564 Proj 1 Dataset.csv").then(data => {
             .attr("class", "axis-title")
             .attr("transform", `rotate(-90)`)
             .attr("x", -innerHeight / 2)
-            .attr("y", -margin.left)
+            .attr("y", -margin.left + 20)
             .style("text-anchor", "middle")
-            .text("Frequency");
+            .text(useLogScale ? "Frequency (log scale)" : "Frequency");
     }
 
     function updateScatterplot() {
